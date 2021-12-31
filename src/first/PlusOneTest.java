01f65f1e1ea48459a03454507e54e31590f38242
唐超超 <tangcc@njhcsoft.com>
2021-12-31 10:46:30 +0800

    加一

---
+++ src/first/PlusOneTest.java
@@ -0,0 +1,25 @@
+package first;
+
+import java.util.Arrays;
+
+public class PlusOneTest {
+    public static void main(String[] args) {
+        int[] digits = new int[]{7, 8, 9};
+        int[] nums = plusOne(digits);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] == 9) {
+                digits[i] = 0;
+            } else {
+                digits[i] = digits[i] + 1;
+                return digits;
+            }
+        }
+        int[] nums = new int[digits.length + 1];
+        nums[0] = 1;
+        return nums;
+    }
+}
